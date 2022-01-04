4e19f50741451618ac73c41a576407157d158e2f
SirVoRaK <sirvoraky@gmail.com>
2022-01-04 02:44:31 -0300

    ping

--- public/modules/workers/ping-worker.js
+++ public/modules/workers/ping-worker.js
@@ -3,20 +3,8 @@
 onmessage = async () => {
     const startTime = new Date().getTime()
     try {
-        const xhr = new XMLHttpRequest()
-        xhr.open('GET', location.protocol + '//' + location.host + '/ping', true)
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) {
-                postMessage(new Date().getTime() - startTime)
-            }
-        }
-        try {
-            xhr.send(null)
-        } catch (error) {
-            postMessage(new Date().getTime() - startTime)
-        }
-        /* await fetch(location.protocol + '//' + location.host + '/ping')
-        postMessage(new Date().getTime() - startTime) */
+        await fetch(location.protocol + '//' + location.host + '/ping')
+        postMessage(new Date().getTime() - startTime)
     } catch (e) {
         postMessage(100)
     }
